25597d676d09f795650462b5020d26ae32d41ba8
Yudas <yudasmalabi@gmail.com>
2022-03-04 15:14:08 +0700

    install laravel ui and some npm packages, import routing and controllers from the previous projects

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProgramController;
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,7 +21,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+Auth::routes();
+
+Route::get('/', [WelcomeController::class, 'index']);
+Route::get('home', [HomeController::class, 'index'])->name('home');
 
 Route::prefix('category')->group(function () {
     Route::get('marbel-edu-games', [ProductController::class, 'eduGames']);
