98e3d64c9083979d63c38916ea4ed5fc10139631
Yudas <yudasmalabi@gmail.com>
2022-03-06 21:46:56 +0700

    done prefix for category view

--- resources/views/pages/programs/index.blade.php
+++ resources/views/pages/programs/index.blade.php
@@ -1,1 +1,67 @@
-ini halaman index program
+@section('title', 'Halaman Kategori')
+@extends('layouts.templates.main')
+@section('main-content')
+<div class="contactus">
+    <div class="container-fluid">
+             <div class="row">
+                <div class="col-md-8 offset-md-2">
+                   <div class="title">
+                      <h2>Kategori Kami</h2>
+                     
+                   </div>
+                </div>
+             </div>
+           </div>
+ </div>
+ <div class="ourproduct">
+   <div class="container">
+      <div class="row product_style_3" ">
+       <!-- product -->
+       <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+         <div class="full product">
+           <div class="product_img">
+             <div class="center"> <img src="{{ asset('assets/icon/p1.png') }}" alt="#"/>
+               <div class="overlay_hover"> <a class="add-bt" href="product_detail3.html">+ Add to cart</a> </div>
+             </div>
+           </div>
+           <div class="product_detail text_align_center">
+             <p class="product_price">$679.89 <span class="old_price">$679.89</span></p>
+             <p class="product_descr">Lightweight Down Puffer Coat</p>
+           </div>
+         </div>
+       </div>
+       <!-- end product -->
+       <!-- product -->
+       <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+         <div class="full product">
+           <div class="product_img">
+             <div class="center"> <img src="{{ asset('assets/icon/p2.png') }}" alt="#"/>
+               <div class="overlay_hover"> <a class="add-bt" href="product_detail3.html">+ Add to cart</a> </div>
+             </div>
+           </div>
+           <div class="product_detail text_align_center">
+             <p class="product_price">$679.89 <span class="old_price">$679.89</span></p>
+             <p class="product_descr">Lightweight Down Puffer Coat</p>
+           </div>
+         </div>
+       </div>
+       <!-- end product -->
+       <!-- product -->
+       <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+         <div class="full product">
+           <div class="product_img">
+             <div class="center"> <img src="{{ asset('assets/icon/p3.png') }}" alt="#"/>
+               <div class="overlay_hover"> <a class="add-bt" href="product_detail3.html">+ Add to cart</a> </div>
+             </div>
+           </div>
+           <div class="product_detail text_align_center">
+             <p class="product_price">$679.89 <span class="old_price">$679.89</span></p>
+             <p class="product_descr">Lightweight Down Puffer Coat</p>
+           </div>
+         </div>
+       </div>
+       <!-- end product -->
+     </div>
+   </div>
+ </div>
+ @endsection
